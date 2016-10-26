422eb2110369dabecec7f6bc734d1fa0c860da76
“Ksuyaresko” <Kiaresko@gmail.com>
2016-10-26 10:02:09 +0300

    new

--- frontend/js/myslider.js
+++ frontend/js/myslider.js
@@ -1,5 +1,33 @@
 //Slider//
+
 define(['jquery', 'slick'], function($) {
+    
+    $('.slider').slick({
+      dots: true,
+  });
+
+
+    $('.benefits__slider').slick({
+        dots: true,
+        arrows: false,
+        mobileFirst: true,
+    responsive: [
+    {
+      breakpoint: 830,
+      settings: "unslick"
+    },
+  ]
+});
+    
+$(window).resize(function() {
+  $('.benefits__slider').slick('resize');
+});
+    
+    
+});
+
+
+/*define(['jquery', 'slick'], function($) {
     
     $('.slider').slick({
       dots: true,
@@ -21,4 +49,4 @@ define(['jquery', 'slick'], function($) {
 $(window).load(ViewUpdate);
 $(window).resize(ViewUpdate);
     
-});
+});*/
